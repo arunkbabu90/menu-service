9a969ffa2b4c303f105cfda7aa2a63d6af2133fc
arunkbabu90 <arunkbabu90@gmail.com>
2024-07-22 16:10:36 +0530

    Initial Commit

---
+++ src/test/kotlin/com/portal/menuservice/MenuServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.portal.menuservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MenuServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
